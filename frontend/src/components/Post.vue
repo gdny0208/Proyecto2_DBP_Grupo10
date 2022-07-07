a99f3b129b4f4ee3b974a3da610b1618446396c7
Giuseppe Del Negro <giuseppe.delnegro@utec.edu.pe>
2022-07-06 23:08:25 -0500

    Nº3

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,26 +1,9 @@
 <template>
-    <div>
-        <input type="text" v-model="textx" placeholder="post" />
+    <div class="create">
+        <h1>Crear Post</h1>
+        <input type="text" v-model="textx" placeholder="Post" /><br>
         <button v-on:click="post1">Post</button>
     </div>
-    <!--<div>
-        <div class="Post">
-            <h1 align="center">Publicaciones</h1>
-            <form>
-                <div align="center">
-                    <textarea name="text1" id="text" class="form-control" v-model="text1"></textarea>
-                </div >
-                <br />
-                <div align="center">
-                    <button v-on:click="post1">Publicar</button>
-                </div>
-            </form>
-            <br />
-            <div align="center">
-                <router-link :to="{name:'Home'}" tag="button">Atrás</router-link>
-            </div>
-        </div>
-    </div>-->
 </template>
 
 <script>
@@ -51,45 +34,34 @@ export default {
 </script>
 
 <style>
-    .Post{
-      background: rgba(0, 0, 0, 0.7) url('https://wallpaperstock.net/libro-de-recetas-wallpapers_53636_1920x1200.jpg');
-      height: 100%;
-      background-position: auto;
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-blend-mode: darken;
+    .create{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgb(81, 105, 224);
     }
-    .Post h1{
-      color: rgb(228, 218, 197);
-      padding-top: 50px;
-      padding-bottom: 50px;
-      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    .create h1{
+      color: rgb(4, 3, 1);
+      padding-top: 60px;
+      padding-bottom: 15px;
+      font-family: 'Raleway',sans-serif;
       font-size: 2.5em;
     }
-    .Post textarea{
-     height: 250px;
-    }
-    .Post.form-control{
-      box-sizing: border-box;
-      border: 3px solid rgb(72, 71, 71);
-      border-radius: 8px;
-      background-color: #f8f8f8;
-      font-size: 16px;
-      resize: none;
-    }
-    .Post input {
-        width: 300px;
-        height: 40px;
+    .create input {
+        height:300px;
+        width:500px;
         padding-left: 20px;
         display: block;
         margin: 20px;
         margin-bottom: 30px;
         margin-right: auto;
         margin-left: auto;
-        border: 1px solid #42b983;
+        border: 3px solid #2011ef;
         border-radius: 5px;
+        font-size: 16px;
     }
-    .Post button {
+    .create button {
         width: 300px;
         height: 40px;
         padding-left: 20px;
@@ -97,9 +69,9 @@ export default {
         margin-bottom: 30px;
         margin-right: auto;
         margin-left: auto;
-        background-color: #42b983;
+        background-color: #2011ef;
         color: #fff;
         border: none;
-        border-radius: 5px;
+        border-radius: 80px;
     }
 </style>
--- frontend/src/components/Ver.vue
+++ frontend/src/components/Ver.vue
@@ -2,11 +2,13 @@
 <div class="Ver">
     <h1 align="center">Publicaciones</h1>
     <div align="center">
-        <router-link class="link" :to="{name:'Blog'}"><button>Create a Post</button> </router-link>
+        <router-link class="link" :to="{name:'Blog'}"><button>Crear Post</button> </router-link>
     </div>
     <div>
-      <li  v-for="item in posts.users" :key="item">      
-      {{ item }}
+      <div class="post"  v-for="item in posts.users" :key="item">  
+        <div class="post-body">
+          <p>{{item.text}}</p>
+        </div>
          <button>
                <a  v-on:click="delete2" class="dropdown-item" >Delete {{delete1 = item.id}} </a>
             </button>
@@ -16,8 +18,8 @@
               </li>
             </ul>
       
-      </li>
-      </div>
+      </div><br>
+    </div>
 </div>
 </template>
 
@@ -54,6 +56,9 @@ export default {
 
 
   <style>
+    .Ver{
+        background-color: rgb(81, 105, 224);
+    }
     .Ver body{
       background: rgba(0, 0, 0, 0.7) url('https://i.pinimg.com/originals/af/db/30/afdb30e83d6ca64eb482a6b40c295e08.jpg');
       height: 100%;
@@ -68,14 +73,49 @@ export default {
       font-weight: bold;
       padding-top: 50px;
       padding-bottom: 20px;
+      font-size: 70px;
+      font-weight: 600;
+      color: white;
+      background-clip: text;
+      -webkit-background-clip: text;
+      font-family: 'Raleway',sans-serif;
     }
-    .card {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    border-radius: 5px; /* 5px rounded corners */
+    .Ver button{
+      background-color: #2011ef;
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      padding-bottom: 15px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+      border-radius: 8px;
     }
-    /* Add rounded corners to the top left and the top right corner of the image */
-    img {
-    border-radius: 5px 5px 0 0;
+    .post {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-top: 5px;
     }
+    .post-body {
+      width: 900px;
+      height: auto;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 50px;
+      border: 4px solid #2011ef;
+      border-radius: 8px;
+      padding-top: 10px;
+      padding-left: 5px;
+      padding-right: 5px;
+      background-color: white;
+      text-align: left;
+      color: black;
+    }
+    
   </style>
